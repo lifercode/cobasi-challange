b8ad2086a75a022a7fc3558d98d6c9e6b31f9b36
Andre Murilo <lifercode@gmail.com>
2021-10-05 18:11:00 -0300

    chore(ongs): add initial mock file of ongs

---
+++ mock/ongs.ts
@@ -0,0 +1,24 @@
+const ongs = [
+  {
+    name: 'SUIPA',
+    description: 'A SUIPA foi inaugurada em 27 de abril de 1943. Com uma renomada história atuando na defesa dos animais no Rio de Janeiro, atualmente a SUIPA mantém um abrigo de animais e presta assistência veterinária a preços populares.'
+  },
+  {
+    name: 'Vira Lata Vira Amigo',
+    description: 'A “Vira Lata Vira Amigo” foi criada oficialmente em setembro de 2016, que se dedica desde 2012 em atividades de proteção, ajuda, e procura de lares a cães da Grande São Paulo. A ONG cuida de aproximadamente 80 cães, todos bem instalados em sua sede própria. Suas feiras de adoção acontecem na Cobasi Tamboré.'
+  },
+  {
+    name: 'Res-gatinhos',
+    description: 'Projeto criado para auxiliar as pessoas a resgatar gatinhos de rua. Compartilham com elas os cuidados que os pets precisam até a adoção. Promove eventos de adoção na Cobasi Iguatemi Ribeirão Preto.'
+  },
+  {
+    name: 'PISA',
+    description: 'A presidente da ONG, Genilda atua há muitos anos na causa animal como protetora independente e ajudando outros protetores. Formalizou seu trabalho há dois anos com a fundação da ONG PISA que atua com resgate e adoções de cães e gatos.'
+  },
+  {
+    name: 'Instituto Luisa Mell',
+    description: 'Fundado em fevereiro de 2015, o Instituto Luisa Mell fica em Ribeirão Pires atua principalmente no resgate de animais feridos ou em situação de risco, recuperação e adoção. Mantemos um abrigo com cerca de 300 animais, entre cães e gatos, onde eles são protegidos, alimentados e aguardam pela chance de serem adotados.'
+  }
+]
+
+export default ongs
